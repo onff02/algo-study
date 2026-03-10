a734fceec6c236f8f82ee27746ae3e344fd278af
onff02 <165152345+onff02@users.noreply.github.com>
2026-03-10 11:21:42 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i*i<=n; i++){
+        if(n%i == 0){
+            if(i*i == n){
+                answer += 1;
+                break;
+            }
+            answer += 2;
+        }
+    }
+    return answer;
+}
