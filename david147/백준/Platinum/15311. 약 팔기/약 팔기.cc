a427e4f7a1fb9e626c002bcd6472aa7630eccf29
Yeongun Choi <dooopro00@gmail.com>
2026-03-14 23:33:55 +0900

    Boj/platinum/15311 (#350)
    * [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub
    
    * auto: merge solved problems into david147 folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ david147/백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF (1 << 28)
+#define N (int) 1e6
+#define mod (long long) 1e9
+
+int n, m, h, maximum = INT_MIN, minimum = INT_MAX, ans;
+bool flag;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    cout << 2000 << '\n';
+    for (int i = 0; i < 1000; ++i) cout << 1 << ' ';
+    for (int i = 0; i < 1000; ++i) cout << 1000 << ' ';
+    cout << '\n';
+
+    return 0;
+}
