8ea61f130d8ca1eb8eafb1d3b3d75f3a389abe16
haeunjeon0410 <janice@sookmyung.ac.kr>
2026-03-18 06:49:32 +0900

    Boj/silver/2164 (#402)
    * [Silver IV] Title: 카드2, Time: 212 ms, Memory: 51848 KB -BaekjoonHub
    
    * auto: merge solved problems into hahahaeun folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ hahahaeun/백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+dq = deque()
+
+for i in range(n):
+    dq.append(i+1)
+
+while len(dq) != 1:
+    dq.popleft()
+    dq.append(dq.popleft())
+
+print(dq[0])
+
