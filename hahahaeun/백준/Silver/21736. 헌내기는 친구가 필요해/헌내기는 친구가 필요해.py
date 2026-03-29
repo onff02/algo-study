faf53f6e3d882990bde5857e668de7c0d9091a24
haeunjeon0410 <janice@sookmyung.ac.kr>
2026-03-30 08:03:42 +0900

    Boj/silver/21736 (#593)
    * [Silver II] Title: 헌내기는 친구가 필요해, Time: 556 ms, Memory: 112184 KB -BaekjoonHub
    
    * auto: merge solved problems into hahahaeun folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ hahahaeun/백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+school = []
+
+nx = [0, 0, 1, -1]
+ny = [1, -1, 0, 0]
+people = 0
+visit = [[False] * m for _ in range(n)]
+
+def meet(x, y):
+    visit[y][x] = True
+    global people
+
+    for i in range(4):
+        di = nx[i] + x
+        dj = ny[i] + y
+
+        if 0 <= di < m and 0 <= dj < n:
+            if not visit[dj][di]:
+
+                if school[dj][di] == 'P':
+                    people += 1
+                
+                if school[dj][di] != 'X':
+                    meet(di,dj)
+
+
+for i in range(n):
+    s = input().strip()
+    if s.find('I') != -1:
+        doyeon_x, doyeon_y = (int(s.find('I')), i)
+    school.append(list(s))
+
+
+meet(doyeon_x, doyeon_y)
+
+if people == 0:
+    print('TT')
+else:
+    print(people)
