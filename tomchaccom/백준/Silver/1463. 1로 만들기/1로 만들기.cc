f2d3bdd9fd3d159894a47884979eb9108cb8ab83
myeongsung kim <dreamkms2014@pusan.ac.kr>
2026-04-02 22:48:09 +0900

    Boj/silver/1463 (#640)
    * 📊 weekly dashboard update
    
    * [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub
    
    * auto: merge solved problems into tomchaccom folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ tomchaccom/백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n;
+
+    cin >> n;
+    vector<int> dp(n+1);
+
+    dp[1] = 0;
+
+    for(int i = 2; i <= n; i++){
+        dp[i] = dp[i -1] + 1;
+
+        if(i % 2 == 0) dp[i] = min(dp[i / 2] + 1, dp[i]);
+        if(i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
+    }
+    cout << dp[n];
+}
